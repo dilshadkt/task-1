b099f9d938b8f15d5ff8916b53b7bfbf1e9898d9
Dilshad <hmydilshadkt@gmail.com>
2024-04-02 17:36:25 +0530

    add logout

--- app/_components/Navbar.jsx
+++ app/_components/Navbar.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { useRouter } from "next/navigation";
 
 const Navbar = () => {
+  const navigate = useRouter();
+  const logout = () => {
+    localStorage.clear();
+    navigate.replace("/auth/login");
+  };
   return (
     <div className="flex items-center justify-end h-[70px] bg-base-200 px-[5%]">
-      <div className="w-10 h-10 rounded-full bg-black"></div>
+      <div
+        onClick={() => logout()}
+        className="w-10 h-10 rounded-full bg-black"
+      ></div>
     </div>
   );
 };
