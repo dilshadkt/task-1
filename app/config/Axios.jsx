ae948795fa5b251a3d9e175decd44accc10c3c6f
Dilshad <hmydilshadkt@gmail.com>
2024-04-03 09:38:37 +0530

    change base url

--- app/config/Axios.jsx
+++ app/config/Axios.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 const instance = axios.create({
-  // baseURL: "https://task-1-server-53s1.onrender.com/",
-  baseURL: "http://localhost:8000",
+  baseURL: "https://task-1-server-53s1.onrender.com/",
+  // baseURL: "http://localhost:8000",
 });
 
 instance.interceptors.request.use(
